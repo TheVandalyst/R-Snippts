0924a6063708b184e5d0006b621f09c1051ca69f
Osama Jamil <osama.jamil@gmail.com>
2019-09-10 16:13:06 +1000

    Update and rename Rselenium Send Keys methods.R to Rselenium_Send_Keys_methods.r

--- Rselenium_Send_Keys_methods.r
+++ Rselenium_Send_Keys_methods.r
@@ -2,17 +2,18 @@
 
 
 #Method 1
-
+#using Java Script
 name <- remDr$findElements("tag", "textarea")
-remDr$executeScript("arguments[0].value='admin'",name[1])
+remDr$executeScript("arguments[0].value='test'",name[1])
 
 #Method 2
-
+#sending KeyStrokes
 name[[1]]$sendKeysToElement(list( key = "enter"))       #if you want to send Enter
 name[[1]]$sendKeysToElement(list(key = 'control', 'v')) #if you want to send Ctrl+V
 name[[1]]$sendKeysToElement(list("text"))               #if you just want to send text 
 
 #Method 3
+#using JavaScript
 remDr$executeScript("document.getElementsByTagName('textarea')[0].value = arguments[0];", list("test"))
 
 
